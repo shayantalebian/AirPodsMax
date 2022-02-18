f0d3c0be754571a2428c4207998ac0644a487839
Shayan Talebian <shayantalebianwork@gmail.com>
2022-02-18 17:30:59 +0330

    Fix minor bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,6 @@ if (navClose) {
 
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll(".nav__link");
-
 function linkAction() {
   const navMenu = document.getElementById("nav-menu");
   // When we click on each nav__link, we remove the show-menu class
