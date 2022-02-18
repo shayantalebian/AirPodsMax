12367076e308d30a5ce49d7b8005b8d3fd6b6496
Shayan Talebian <shayantalebianwork@gmail.com>
2022-02-18 17:25:25 +0330

    Create header and nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,38 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
+/* ===== MENU SHOW  ===== */
+// Validate if constant exist
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/* ===== MENU CLOSE  ===== */
+// Validate if constant exist
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
 
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
+/*==================== SHOW SCROLL UP ====================*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
