d08806d49ff9c3c25db1e2024e9297ba95679b98
Shayan Talebian <shayantalebianwork@gmail.com>
2022-03-02 09:55:57 +0330

    Make header sticky in scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,9 +51,10 @@ if (arrowUp.classList.contains("hidden")) {
 }
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
-/*==================== SHOW SCROLL UP ====================*/
-
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
