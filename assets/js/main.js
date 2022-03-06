9eb0c0af59a40629a266c3d8da434ea7e7f1809f
Shayan Talebian <shayantalebianwork@gmail.com>
2022-03-06 10:24:00 +0330

    Fix some bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,3 +52,28 @@ for (let i = 0; i < links.length; i++) {
     this.className += " active";
   });
 }
+
+// const sections = document.querySelectorAll("section[id]");
+// console.log(sections);
+
+// function scrollActive() {
+//   const scrollY = window.pageYOffset;
+
+//   sections.forEach((current) => {
+//     const sectionHeight = current.offsetHeight;
+//     const sectionTop = current.offsetTop - 50;
+//     sectionId = current.getAttribute("id");
+
+//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//       document
+//         .querySelector(".nav__menu a[href*=" + sectionId + "]")
+//         .classList.add("active");
+//     } else {
+//       document
+//         .querySelector(".nav__menu a[href*=" + sectionId + "]")
+//         .classList.remove("active");
+//     }
+//   });
+
+//   window.addEventListener("scroll", scrollActive);
+// }
